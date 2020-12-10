2e0b5de53f57a4a9e13008424a5a9abc2b2fafdc
Aarfeen1 <72484730+Aarfeen1@users.noreply.github.com>
2020-12-10 10:41:17 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,18 @@
+package com.xworkz.StarPatterns;
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		int num=6;
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j < i+1; j++) {
+			
+				System.out.print("*");
+				
+				
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
